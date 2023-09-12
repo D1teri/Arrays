a870149fb2c11fe9c83acfdea7c2823f425e9f80
Dmytro Karpenko <dmytro.karpenko@mrjaffesclass.com>
2023-09-12 11:42:56 -0700

    Arrays

---
+++ Arrays.java
@@ -0,0 +1,17 @@
+
+/**
+ Author: D1teri or Dima K.
+ Version: 1.0.0
+ */
+public class Arrays
+{
+    
+    
+    public void run(){
+        
+    }
+    public static void main (String[] args){
+        Arrays sp = new Arrays();
+        sp.run();
+    }
+}
