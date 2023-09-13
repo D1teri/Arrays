0defbc31126823883841cd01280085c498051716
Dmytro Karpenko <dmytro.karpenko@mrjaffesclass.com>
2023-09-13 11:37:23 -0700

    Arrays

--- Arrays.java
+++ Arrays.java
@@ -5,10 +5,22 @@
  */
 public class Arrays
 {
-    
+    String[] strArray = {"Alpha", "Bravo", "Charlie", "Delta", "Echo"};
     
     public void run(){
-        
+        for(int i = 0; i < strArray.length; i++){
+            System.out.println(strArray[i]);
+        }
+        for(String value : strArray){
+            System.out.print(value + " ");
+        }
+        System.out.println(" ");
+        for(int i = 0; i < strArray.length; i++){
+            System.out.print(strArray[i]);
+            if(i < (strArray.length - 1)){
+                System.out.print("-");
+            }
+        }
     }
     public static void main (String[] args){
         Arrays sp = new Arrays();
